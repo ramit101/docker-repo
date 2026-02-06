6ba7f523f89d5dd75303675253c0479deed53344
greatGaurav101 <gauravpaul549@gmail.com>
2026-02-06 21:28:22 +0530

    first commit for docker

---
+++ src/main/java/com/girnarsoft/controller/getController.java
@@ -0,0 +1,13 @@
+package com.girnarsoft.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class getController {
+
+    @RequestMapping("/api/v1/message")
+    public String getMessage() {
+        return "message from controller of microservice for docker";
+    }
+}
